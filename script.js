072e1eedddbd16c13063a95fd8b2504d591b1e64
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-20 22:59:58 -0300

    at?

--- script.js
+++ script.js
@@ -4,3 +4,20 @@
 // btn.addEventListener('click', () => {
 //   ac.classList.add('win')
 // })
+
+let count = 1
+document.getElementById("radio1").checked = true;
+  nextImg();
+setInterval( function(){
+
+}, 2000)
+
+function nextImg(){
+  count++;
+  if(count>4){
+    count = 1;
+  }
+
+  document.getElementById("radio1"+count).checked = true;
+
+}
