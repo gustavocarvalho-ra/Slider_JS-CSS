75db1dfa52ce2d1b3df7a2a03542f6b32bd44405
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-20 22:55:47 -0300

    at?

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const ac = document.querySelector('.slide')
-const btn = document.querySelector('.win')
+// const ac = document.querySelector('.slide')
+// const btn = document.querySelector('.win')
 
-btn.addEventListener('click', () => {
-  ac.classList.add('win')
-})
+// btn.addEventListener('click', () => {
+//   ac.classList.add('win')
+// })
